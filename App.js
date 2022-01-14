80871a090bb6ac19e9ed2d0a781951798679c941
Moabe <moabeluis2018@gamil.com>
2022-01-13 21:08:00 -0300

    Bug consertado

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { StyleSheet,  View } from 'react-native';
 import Title from "./src/components/Title"
-import Main from "./src/components/Main"
+import Form from "./src/components/Form"
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title/>
-      <Main/>
+      <Form/>
     </View>
   )
 }
